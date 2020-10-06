8d19e3942d1d8dc0724cfe46d4813108b21fc73d
Coen Townson <coen.townson21@gmail.com>
2020-10-07 01:41:42 +1100

    JAJAC-16 First pass at password reset backend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
-import os
 import json
+import os
+import smtplib
+from json import dumps
+
 from flask import Flask, render_template, request
 from flask_cors import CORS
+from flask_mail import Mail, Message
 from flask_pymongo import PyMongo
-from json import dumps
+
+import password_reset as password_reset
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +17,16 @@ CORS(app)
 # app.config["MONGO_URI"] = "mongodb://localhost:27017/angular-flask-muckaround"
 app.config["MONGO_URI"] = "mongodb://jajac:databasepassword@coen-townson.me:27017/angular-flask-muckaround?authSource=admin"
 mongo = PyMongo(app)
+
+# Creating email server
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME = "photopro.jajac@gmail.com",
+    MAIL_PASSWORD = "photoprodemopassword"
+)
+
 
 @app.route('/start', methods=['GET', 'POST'])
 def send_data():
@@ -31,6 +46,33 @@ def send_data():
         'first_name' : first_name,
         'colour': colour
     })
+
+@app.route('/passwordreset/request', methods=['GET','POST'])
+def auth_password_reset_request():
+    """
+    Given an email address, if the user is a registered user, semd an email
+    with a link that they can access temporarily to change their password
+    """
+    print(request.values.get("email"))
+    # data = json.loads(request.args)
+    email = request.values.get("email")
+
+    msg = password_reset.password_reset_request(email)
+    mail = Mail(app)
+    mail.send(msg)
+
+    return dumps({})
+
+@app.route('/passwordreset/reset', methods=['POST'])
+def auth_passwordreset_reset():
+    """ Given a reset code, change user's password """
+
+    reset_code = request.form.get("reset_code")
+    new_password = request.form.get("new_password")
+
+    return dumps(
+        password_reset.password_reset_reset(reset_code, new_password)
+    )
 
 if __name__ == '__main__':
     app.run(port=8001, debug=True)
