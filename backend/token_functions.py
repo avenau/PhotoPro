406818d080e04b96cb10bc78a4925e8903168029
Jaczel <j.aczel@student.unsw.edu.au>
2020-10-16 10:23:13 +1100

    Cleaned up token_functions and added more error handling

--- backend/token_functions.py
+++ backend/token_functions.py
@@ -1,15 +1,33 @@
 import jwt
+from jwt.exceptions import DecodeError
+from jwt.exceptions import InvalidTokenError
+from jwt.exceptions import InvalidSignatureError
 
 secret = "imnotsurewhattomakethis"
 
-# Create a unique JSON Web Token which is a function of the user's u_id and the secret defined above.
+
 def create_token(u_id):
+    '''
+    Create a unique JSON Web Token which is a function of
+    the user's u_id and the secret defined above.
+    '''
     return jwt.encode({"u_id": u_id}, secret).decode()
 
-def verify_token(token):
-    try:
-        jwt.decode(token, secret)
-    except:
-        return False
 
-    return True
+def verify_token(token):
+    '''
+    Returns the u_id of the token, otherwise raises an exception
+    '''
+    try:
+        u_id = jwt.decode(token, secret)
+    except DecodeError:
+        print("Token failed validation")
+        raise DecodeError
+    except InvalidTokenError:
+        print("Token failed validation")
+        raise InvalidTokenError
+    except InvalidSignatureError:
+        print("Invalid signature" + secret)
+        raise InvalidSignatureError
+
+    return u_id
