78dfbc554d7f9ce4aceb867d689dd6b2a6ef6f13
Joanne <joanne.li3@outlook.com>
2020-10-17 20:47:34 +1100

    add changes

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from flask_mail import Mail
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 from bson.objectid import ObjectId
-from Error import UserDNE, EmailError, PasswordError
+from Error import UserDNE
 from showdown.get_images import get_images
 from welcome.contributors import get_popular_contributors_images
 from welcome.popular_images import get_popular_images
@@ -100,23 +100,15 @@ def verify_token():
 def process_login():
     email = request.form.get("email")
     password = request.form.get("password")
-    if email == "":
-        raise EmailError("Please enter an email address.")
-    if password == "":
-        raise PasswordError("Please enter a password.")
-
-    user = mongo.db.users.find_one({"email": email})
-    if not user:
-        raise EmailError("That email isn't registered.")
-    hashedPassword = user["password"]
-
     u_id = ""
     token = ""
+    user = mongo.db.users.find_one({"email": email})
+    hashedPassword = user["password"]
+
+    # TODO: set the token properly with jwt
     if bcrypt.check_password_hash(hashedPassword, password):
         u_id = user["_id"]
         token = token_functions.create_token(str(u_id))
-    else:
-        raise PasswordError("That password is incorrect.")
 
     return {
         "u_id": str(u_id),
@@ -259,7 +251,7 @@ def manage_account():
     try:
         find_userdb = {"_id": ObjectId(current_user)}
         for key, value in data.items():
-            if (value == "" or key == "u_id"):
+            if (value == ""):
                 continue
             change_userdb = {"$set": { key: value } }
             mongo.db.users.update_one(find_userdb, change_userdb)
@@ -315,7 +307,7 @@ def get_user():
     data['nickname'] = current_user['nickname']
     data['dob'] = current_user['DOB']
     data['location'] = current_user['location']
-    data['aboutMe'] = current_user['aboutMe']
+    data['about_me'] = current_user['aboutMe']
 
     return data
 
