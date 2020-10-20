d73041d595ce484282c4a5db9c4995c1cb645a53
Aven <avenau@live.com>
2020-10-20 16:16:54 +1100

    Done some frontend for photo profiles

--- frontend/src/components/PhotoComments/PhotoComments.tsx
+++ frontend/src/components/PhotoComments/PhotoComments.tsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 import { Button, Col, Form } from "react-bootstrap";
+import CommentMessage from "./CommentMessage";
 import "./PhotoComments.scss";
 
 export default function PhotoComments() {
     const [comments, setComments] = useState(0);
+    let fakeDate = new Date(50000000)
     return (
         <div className="PhotoComments">
             <Form>
-                <Form.Row>
+                <Form.Row id="commentTextArea">
                     <Col>
                         <Form.Control as="textarea" rows={4} placeholder="Add a comment..." />
-                        <Button variant="primary" type="submit">
+                    </Col>
+                    <Col>
+                        <Button variant="primary" type="submit" className="commentButton">
                             Comment
                         </Button>
                     </Col>
+                </Form.Row>
+                <Form.Row>
+                    <CommentMessage message="Hello" author="dollaking" datePosted={fakeDate} />
                 </Form.Row>
             </Form >
 
