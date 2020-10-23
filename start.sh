56c0000162cb80572bc952e45b196181c569e9c3
Coen Townson <coen.townson21@gmail.com>
2020-10-23 15:49:54 +1100

    Add dev mode to start script

--- start.sh
+++ start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if ! [ $1 = "-d" ]
+then
+  port_f=5001
+  while [[ `netstat -taln | egrep $port_f` != "" ]]
+  do
+    echo "Failed on port $port_f, trying another"
+    port_f=$((port_f + 1))
+  done
+  echo "========== Running frontend on port $port_f =============="
+
+  ./utils/run_front.sh $port_f $port_b &
+  sleep 0.1
+fi
+
 port_b=8001
 while [[ `netstat -taln | egrep $port_b` != "" ]]
 do
@@ -7,15 +21,11 @@ do
   port_b=$((port_b + 1))
 done
 
-port_f=5001
-while [[ `netstat -taln | egrep $port_f` != "" ]]
-do
-  echo "Failed on port $port_f, trying another"
-  port_f=$((port_f + 1))
-done
-echo "========== Running frontend on port $port_f =============="
+if [ $1 = "-d" ]
+then
+  echo "var BACKEND_PORT = $port_b;" > frontend/public/config.js
+fi
+
 echo "========== Running backend on port $port_b =============="
 
-./utils/run_front.sh $port_f $port_b &
-sleep 0.1
 ./utils/run_back.sh $port_b
