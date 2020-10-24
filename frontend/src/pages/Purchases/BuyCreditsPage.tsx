42e9378d656c1520ac99eba9362043e3a1c68c65
Al <z5165180@cse.unsw.edu.au>
2020-10-24 15:35:34 +1100

    Progress on Purchases front end

--- frontend/src/pages/Purchases/BuyCreditsPage.tsx
+++ frontend/src/pages/Purchases/BuyCreditsPage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import { RouteChildrenProps } from "react-router-dom";
 import Toolbar from "../../components/Toolbar/Toolbar";
 import axios from "axios";
+import "./BuyCreditsPage.css";
 
 const CURRENCY_API_URL =
   "https://openexchangerates.org/api/latest.json?app_id=";
@@ -15,6 +19,9 @@ class BuyCreditsPage extends React.Component<RouteChildrenProps, any> {
     super(props);
     this.state = {
       credits: 0,
+      priceMsg: "",
+      creditsInput: "",
+      creditsErrMsg: "",
     };
   }
 
@@ -38,11 +45,38 @@ class BuyCreditsPage extends React.Component<RouteChildrenProps, any> {
     return (
       <div className="buyCreditsPage">
         <Toolbar />
-        <Container>
+        <Container className="mt-5">
           <Row>
-            <Jumbotron>Hey</Jumbotron>
-            <Jumbotron>Hey2</Jumbotron>
+            <Col>
+              <Jumbotron id="creditsJumbotron">
+                <h1>How many credits would you like to purchase?</h1>
+                <h3>
+                  <i>You currently have {this.state.credits} Credits.</i>
+                </h3>
+              </Jumbotron>
+            </Col>
           </Row>
+          <Form>
+            <Form.Group as={Row} controlId="credits">
+              <Col xs={3}>
+                <Form.Label>Number of Credits:</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control type="number"></Form.Control>
+                <Form.Text className="text-muted">
+                  <p className="error">Error here</p>
+                </Form.Text>
+              </Col>
+            </Form.Group>
+            <Row id="buttonsRow" className="mt-5">
+              <Col>
+                <Button size="lg">Pay with PayPal</Button>
+              </Col>
+              <Col>
+                <Button size="lg">Pay with Card</Button>
+              </Col>
+            </Row>
+          </Form>
         </Container>
       </div>
     );
