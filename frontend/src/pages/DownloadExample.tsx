8e59126d0a3330787cdafdd58102d7a5bbab94ef
Coen Townson <coen.townson21@gmail.com>
2020-10-30 10:50:56 +1100

    Document download

--- frontend/src/pages/DownloadExample.tsx
+++ frontend/src/pages/DownloadExample.tsx
@@ -4,6 +4,8 @@ import Axios from "axios";
 
 class DownloadExample extends React.Component {
   onClick() {
+    // This backend route no longer exists but a route just needs to be made
+    // to return base64 string of an image and this component will make it a DL
     Axios.get("/download").then((r) => {
       const link = document.createElement("a");
       link.href = `data:image/jpg;base64,${r.data}`;
