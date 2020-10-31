7fc263e46be8644c7957ac50216bdc3408061527
Jaczel <jaczel@fastmail.com.au>
2020-10-31 21:57:41 +1100

    General lint fixes

--- backend/lib/photo/photo_edit.py
+++ backend/lib/photo/photo_edit.py
@@ -4,20 +4,24 @@ Create and modify photos which are uploaded by a user
 """
 import base64
 import datetime
+from io import BytesIO
 from bson.objectid import ObjectId
 from PIL import Image
-from io import BytesIO
 
 
-from lib.photo.validate_photo import validate_photo, validate_photo_user, reformat_lists, validate_extension, validate_discount
-from ..token_functions import get_uid
+from lib.photo.validate_photo import validate_photo
+from lib.photo.validate_photo import validate_photo_user
+from lib.photo.validate_photo import reformat_lists
+from lib.photo.validate_photo import validate_extension
+from lib.photo.validate_photo import validate_discount
+from lib.token_functions import get_uid
 from lib.photo.fs_interactions import find_photo, save_photo
 
 
 def create_photo_entry(mongo, photo_details):
     """
-    Creates photo entry in photo collection and adds photo/post in the user collection
-
+    Creates photo entry in photo collection and adds
+    photo/post in the user collection
     """
 
     photo_details = reformat_lists(photo_details)
@@ -25,7 +29,7 @@ def create_photo_entry(mongo, photo_details):
     validate_extension(photo_details["extension"])
 
     # Get photo values before popping them
-    [ metadata, base64_str ] = photo_details['photo'].split(',')
+    [metadata, base64_str] = photo_details['photo'].split(',')
     extension = photo_details['extension']
     photo_details.pop("photo")
 
@@ -53,10 +57,12 @@ def create_photo_entry(mongo, photo_details):
         process_photo(base64_str, name, extension)
 
         # Add photo to user's posts
-        response = mongo.db.users.find_one({"_id": ObjectId(user_uid)}, {"posts": 1})
+        response = mongo.db.users.find_one({"_id": ObjectId(user_uid)},
+                                           {"posts": 1})
         posts = response["posts"]
         posts.append(ObjectId(name))
-        mongo.db.users.update_one({"_id": ObjectId(user_uid)}, {"$set": {"posts": posts}})
+        mongo.db.users.update_one({"_id": ObjectId(user_uid)},
+                                  {"$set": {"posts": posts}})
         return {
             "success": "true"
         }
@@ -66,6 +72,7 @@ def create_photo_entry(mongo, photo_details):
         return {
             "success": "false"
         }
+
 
 def process_photo(base64_str, name, extension):
     """
@@ -88,14 +95,14 @@ def process_photo(base64_str, name, extension):
         thumb.thumbnail((300, 200))
         buffer = BytesIO()
         thumb.save(buffer, thumb.format)
-        save_photo(base64.b64encode(buffer.getvalue()).decode("utf-8"), filename_thumbnail)
+        save_photo(base64.b64encode(buffer.getvalue()).decode("utf-8"),
+                   filename_thumbnail)
 
 
-
-# Get details about photo
 def get_photo_edit(mongo, photoId, token):
     """
-    Get photo details from the database, validates if user is authorised to edit the photo
+    Get photo details from the database,
+        validates if user is authorised to edit the photo
     @param mongo(object): Mongo databse
     @param photoId: str
     @param token: str
@@ -141,7 +148,8 @@ def update_photo_details(mongo, photo_details):
     validate_photo_user(mongo, photoId, user_uid)
 
     for i in modify:
-        mongo.db.photos.update({"_id": ObjectId(photoId)}, {"$set": {i: photo_details[i]}})
+        mongo.db.photos.update({"_id": ObjectId(photoId)},
+                               {"$set": {i: photo_details[i]}})
 
     return {
         "success": "true"
