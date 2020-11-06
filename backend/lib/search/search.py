59e5570c08e496bb76264a016e252a455c7050da
Allan <allan.chen1998@gmail.com>
2020-11-06 14:01:00 +1100

    Fix search functionality for anon users

--- backend/lib/search/search.py
+++ backend/lib/search/search.py
@@ -124,11 +124,17 @@ def photo_search(data):
         ]
     )
     res = loads(dumps(res))
-    req_user_obj = User.objects.get(id=req_user)
+
+    # If signed in
+    if req_user:
+        req_user_obj = User.objects.get(id=req_user)
+
     for result in res:
+        if req_user:
+            result["owns"] = (cur_photo in req_user_obj.get_all_purchased()) or (cur_photo.is_photo_owner(req_user_obj))
         cur_photo = Photo.objects.get(id=result["id"])
         result["photoStr"] = cur_photo.get_thumbnail(req_user)
-        result["owns"] = (cur_photo in req_user_obj.get_purchased()) or (cur_photo.is_photo_owner(req_user_obj))
+        
 
     return res
 
