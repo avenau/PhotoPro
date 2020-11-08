11e8f1b3729d7e159f6e9a7b096c40db55c24197
Aven <avenau@live.com>
2020-11-08 14:24:06 +1100

    Cleaned Up Profile Details on app.py

--- backend/app.py
+++ backend/app.py
@@ -46,6 +46,7 @@ from lib.photo.remove_photo import remove_photo
 
 # Photo details
 from lib.photo_details.photo_likes import is_photo_liked, like_photo
+from lib.photo_details.photo_details import photo_detail_results
 
 
 # Profile
@@ -1343,56 +1344,8 @@ def _photo_details():
     }
     """
     photo_id = request.args.get("p_id")
-    try:
-        req_user = token_functions.get_uid(request.args.get("token"))
-    except:
-        req_user = ""
-    try:
-        _photo = photo.Photo.objects.get(id=photo_id)
-    except photo.Photo.DoesNotExist:
-        print("INVALID!!!!")
-    user_purchasers = user.User.objects(purchased=_photo.id).count()
-    purchased = user_purchasers > 0
-
-    is_artist = str(_photo.get_user().get_id()) == req_user
-    if purchased == False and is_artist == False and _photo.is_deleted() == True:
-        return dumps(
-            {
-                "u_id": "",
-                "title": "",
-                "likes": "",
-                "tagsList": "",
-                "nickname": "",
-                "email": "",
-                "purchased": False,
-                "metadata": "",
-                "price": "",
-                "discount": "",
-                "deleted": _photo.is_deleted(),
-                "photoStr": "",
-                "status": 1,
-                "is_artist": is_artist,
-            }
-        )
-
-    return dumps(
-        {
-            "u_id": str(_photo.get_user().get_id()),
-            "title": _photo.get_title(),
-            "likes": _photo.get_likes(),
-            "tagsList": _photo.get_tags(),
-            "nickname": _photo.get_user().get_nickname(),
-            "email": _photo.get_user().get_email(),
-            "purchased": purchased,
-            "metadata": _photo.get_metadata(),
-            "price": _photo.get_price(),
-            "discount": _photo.get_discount(),
-            "deleted": _photo.is_deleted(),
-            "photoStr": _photo.get_thumbnail(req_user),
-            "status": 1,
-            "is_artist": is_artist,
-        }
-    )
+    token = request.args.get("token")
+    return photo_detail_results(photo_id, token)
 
 @app.route('/photo_details/isLiked', methods=['GET'])
 @validate_token
