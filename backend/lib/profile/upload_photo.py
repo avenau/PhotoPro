b2372ea9e9528757dddd68ec3d8624a11da7d46b
Coen Townson <coen.townson21@gmail.com>
2020-11-10 02:17:58 +1100

    Make profile pictures a *tiny* bit not as horrible to look at

--- backend/lib/profile/upload_photo.py
+++ backend/lib/profile/upload_photo.py
@@ -1,7 +1,7 @@
-'''
+"""
 Create a thumbnail of an image, convert it to base64 and attach
 it to the user
-'''
+"""
 
 import os
 import shutil
@@ -10,9 +10,9 @@ from PIL import Image
 
 
 def extension_to_mimetype(extension):
-    '''
+    """
     Return mimetype based on given extension
-    '''
+    """
     mime = ""
     if extension == ".jpg" or extension == ".jpeg":
         mime = "JPEG"
@@ -20,36 +20,39 @@ def extension_to_mimetype(extension):
         mime = "PNG"
     elif extension == ".svg":
         mime = "SVG+XML"
-    
+
     return mime
 
+
 def update_user_thumbnail(base64_image, extension):
-    '''
+    """
     MAIN CALLABALE FUNCTION
     Modified by Allan to take a base64 string as opposed to url
-    '''
+    """
     mime = extension_to_mimetype(extension)
-    base64_image = base64_image.split(',')[1]
+    base64_image = base64_image.split(",")[1]
     print(mime)
     # Cannot compress SVG
     if mime == "SVG+XML":
         return (base64_image, mime)
-    
+
     # Not SVG
-    size = (150, 150)
+    size = (200, 200)
     # Remove metadata then decode
     img_data = base64.b64decode(base64_image)
 
-    temp_file = open('temp_thumb_uncompressed'+extension, 'wb')
+    temp_file = open("temp_thumb_uncompressed" + extension, "wb")
     temp_file.write(img_data)
     temp_file.close()
-    thumb = Image.open('temp_thumb_uncompressed'+extension)
+    thumb = Image.open("temp_thumb_uncompressed" + extension)
     thumb.thumbnail(size)
-    thumb.save('temp_thumb_compressed'+extension)
+    thumb.save("temp_thumb_compressed" + extension)
     # Attach the following to db entry
-    thumb_b64 = base64.b64encode(open('temp_thumb_compressed'+extension, 'rb').read()).decode('utf-8')
+    thumb_b64 = base64.b64encode(
+        open("temp_thumb_compressed" + extension, "rb").read()
+    ).decode("utf-8")
 
-    os.remove('temp_thumb_uncompressed'+extension)
-    os.remove('temp_thumb_compressed'+extension)
+    os.remove("temp_thumb_uncompressed" + extension)
+    os.remove("temp_thumb_compressed" + extension)
 
     return (thumb_b64, mime)
