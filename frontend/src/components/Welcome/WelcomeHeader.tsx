db2274f2aa2f8f20e900e1c783b3adb3121e014d
Coen Townson <coen.townson21@gmail.com>
2020-11-10 15:31:36 +1100

    Remove previous showdown from welcome page header

--- frontend/src/components/Welcome/WelcomeHeader.tsx
+++ frontend/src/components/Welcome/WelcomeHeader.tsx
@@ -1,56 +1,13 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import {
-  Container,
-  Jumbotron,
-  Row,
-  Col,
-  Image,
-} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import { Jumbotron } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-interface WelcomeHeaderState {
-    winnerPath: string
-}
-
-class WelcomeHeader extends Component<{}, WelcomeHeaderState> {
-  constructor(props:any){
-    super(props);
-    this.state = {
-      winnerPath: '',
-    }
-  }
-
-  componentDidMount(){
-      axios.get('/showdown/getwinner')
-      .then((res) => {
-        this.setState ({
-          winnerPath: res.data.path,
-        });
-      });
-  }
-
+class WelcomeHeader extends Component {
   render() {
     return (
-      <div>
-        <Container>
-          <Row>
-            <Col sm={2}>
-              <Row>
-                <p>Yesterday&apos;s Showdown Winner:</p>
-              </Row>
-              <Row>
-                <Image src={this.state.winnerPath} />
-              </Row>
-            </Col>
-            <Col sm={10}>
-              <Jumbotron>
-                <h1 style={{ textAlign: 'center' }}>Welcome to PhotoPro</h1>
-              </Jumbotron>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Jumbotron>
+        <h1 style={{ textAlign: "center" }}>Welcome to PhotoPro</h1>
+      </Jumbotron>
     );
   }
 }
