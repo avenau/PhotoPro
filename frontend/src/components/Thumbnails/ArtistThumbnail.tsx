f12974b845f04c4fe1ab84c06bb0c839ee76cf7e
Joe Aczel <jaczel@fastmail.com.au>
2020-11-14 20:52:20 +1100

    Restored Artist Lists

---
+++ frontend/src/components/Thumbnails/ArtistThumbnail.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Image from "react-bootstrap/Image";
+import "./ArtistThumbnail.scss";
+import _ from "lodash";
+import profilePic from "../../static/profile-pic.png";
+
+interface Props {
+  name: string;
+  user: string;
+  artistImg: string;
+}
+
+export default class ArtistThumbnail extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  private getPic() {
+    if (_.isEqual(this.props.artistImg, ["", ""])) {
+      return profilePic;
+    }
+    if (this.props.artistImg !== undefined) {
+      // base64 of the tuple profilePic
+      const b64 = `${this.props.artistImg[0]}`;
+      const header = "data:image/";
+      // Filetype of the tuple profilePic
+      const filetype = `${this.props.artistImg[1]}`;
+      const footer = ";base64, ";
+      const ret = header.concat(filetype.concat(footer.concat(b64)));
+
+      return ret;
+    }
+    return profilePic;
+  }
+
+  render() {
+    return (
+      <>
+        <Image src={this.getPic()} className="artist-thumbnail" />
+        <div className="artist-overlay">
+          <div>{this.props.name}</div>
+        </div>
+      </>
+    );
+  }
+}
+
