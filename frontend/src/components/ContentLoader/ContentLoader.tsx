ffbd41d5909b7153f7dcb56b1fdd7ea93cd6f267
Allan <allan.chen1998@gmail.com>
2020-11-14 18:30:17 +1100

    Remove unused ArtistList and ArtistThumbnail

--- frontend/src/components/ContentLoader/ContentLoader.tsx
+++ frontend/src/components/ContentLoader/ContentLoader.tsx
@@ -6,7 +6,6 @@ import AlbumList from "../Lists/AlbumList";
 import CollectionList from "../Lists/CollectionList";
 import PhotoList from "../Lists/PhotoList";
 import UserList from "../Lists/UserList";
-import ArtistList from "../Lists/ArtistList";
 import NoContent from "./NoContent";
 
 interface Props {
@@ -115,8 +114,6 @@ export default class ContentLoader extends React.Component<Props, State> {
         return <CollectionList collections={this.state.results} />;
       case "user":
         return <UserList users={this.state.results} />;
-      case "artist":
-        return <ArtistList artists={this.state.results} />;
       case "albumPhotos":
         return (
           <PhotoList
