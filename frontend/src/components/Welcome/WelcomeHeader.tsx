03b852222523446ccaba2059350fc65ef022983a
Joe Aczel <jaczel@fastmail.com.au>
2020-11-14 22:07:03 +1100

    Added our branding to the Welcome page

--- frontend/src/components/Welcome/WelcomeHeader.tsx
+++ frontend/src/components/Welcome/WelcomeHeader.tsx
@@ -6,6 +6,7 @@ class WelcomeHeader extends Component {
     return (
       <Jumbotron>
         <h1 style={{ textAlign: "center" }}>Welcome to PhotoPro</h1>
+        <p style={{ textAlign: "center" }}> by JAJAC</p>
       </Jumbotron>
     );
   }
