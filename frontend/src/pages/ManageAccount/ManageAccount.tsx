ef83b545360f1a0cd09b597ae894642f40c60466
Allan Chen <allan.chen1998@gmail.com>
2020-11-16 16:33:52 +1100

    Add BackButton

--- frontend/src/pages/ManageAccount/ManageAccount.tsx
+++ frontend/src/pages/ManageAccount/ManageAccount.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Button, Container, Form, Modal } from "react-bootstrap";
 import UserDetails from "../../components/AccountManagement/UserDetails";
 import LoadingButton from "../../components/LoadingButton/LoadingButton";
-import Toolbar from "../../components/Toolbar/Toolbar";
+import BackButton from "../../components/BackButton/BackButton";
 import countries from "../../constants";
 import "./ManageAccount.scss";
 
@@ -122,14 +122,14 @@ export default function ManageAccount(props: any) {
           extension: response[1] ? response[1] : originalExtension,
           token: localStorage.getItem("token"),
         })
-        .then((r:any) => {
+        .then((r: any) => {
           if (r.status !== 200) {
             throw new Error();
           }
           setLoading(false);
           const id = localStorage.getItem("u_id");
-          if(r.data.nickname)
-            localStorage.setItem('nickname', r.data.nickname);
+          if (r.data.nickname)
+            localStorage.setItem("nickname", r.data.nickname);
           props.history.push(`/user/${id}`);
         })
         .catch(() => {
@@ -162,9 +162,12 @@ export default function ManageAccount(props: any) {
 
   return (
     <>
-      <br />
+      <BackButton
+        href={`/user/${localStorage.getItem("u_id")}`}
+        label="Back to Profile"
+      />
       <Container>
-        <h1>Change account details</h1>
+        <h1>Change Account Details</h1>
         <UserDetails
           validateFeedback={validateFeedback}
           validPassword={validPassword}
